c1492c3803dcd593599506045533def9a4ac00e4
MarceloMachadoxD <mastermanson@gmail.com>
2021-05-09 13:36:36 -0300

    frist commit

---
+++ src/main/java/com/github/com/marcelomachadoxd/dcmavelheroes/DcMavelHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.github.com.marcelomachadoxd.dcmavelheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcMavelHeroesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcMavelHeroesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/com/marcelomachadoxd/dcmavelheroes/DcMavelHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.com.marcelomachadoxd.dcmavelheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcMavelHeroesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
